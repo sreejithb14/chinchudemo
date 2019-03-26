019698c02e4e78055d881a301a21cdb13754f3f5
sreejithb14 <sreejith.b@seqato.com>
2019-03-26 12:09:11 +0530

    fisrt

---
+++ chinchudemo/src/main/java/com/example/demo/ChinchudemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+@Configuration
+@EnableAspectJAutoProxy
+@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.example.demo"})
+public class ChinchudemoApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinchudemoApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ChinchudemoApplication.class);
+	}
+}
